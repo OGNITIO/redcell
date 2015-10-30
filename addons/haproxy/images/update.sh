77e1baf9ce0b94e07492274d541fa280c59b26a7
Lucien R. Zagabe <zagabe.lu@gmail.com>
2015-10-30 04:32:26 +0100

    add haproxy docker images definition

---
+++ addons/haproxy/images/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true
+do
+    python /usr/local/bin/servicerouter.py \
+           --marathon http://$MARATHON_URL:$MARATHON_PORT \
+           --haproxy-config /etc/haproxy/haproxy.cfg
+    sleep 42
+done
